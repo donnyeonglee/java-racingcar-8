8fb80260ca15eb98bda12e492b7185bdee517a2c
donnyeonglee <dlehssud@snu.ac.kr>
2025-10-23 15:46:34 +0900

    feat: 입력 받은 자동차 이름과 시도할 횟수에 따른 차수별 실행 기능 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -14,5 +14,34 @@ public class Application {
         // 2. 시도할 횟수를 입력
         int attemptNum = enterVariable.enterAttemptNum();
         System.out.println("시도할 횟수 : " + attemptNum); // 테스트 출력
+
+        // 3. 자동차 List 생성
+        List<Car> carList = new ArrayList<Car>();
+        for (String name : nameList) {
+            Car car = new Car(name);
+            carList.add(car);
+        }
+
+        // 4. 차수별 실행 결과 출력
+        System.out.println();
+        System.out.println("실행 결과");
+        int num;
+        for (num = 1; num <= attemptNum; num++) {
+            attemptAllCars(carList);
+            printCarPosition(carList);
+            System.out.println();
+        }
+    }
+
+    private static void attemptAllCars(List<Car> carList) {
+        for (Car car : carList) {
+            car.attempt();
+        }
+    }
+
+    private static void printCarPosition(List<Car> carList) {
+        for (Car car : carList) {
+            System.out.println(car);
+        }
     }
 }
