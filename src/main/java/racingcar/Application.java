2ec0d056ec1749f0eb3a491c607021908bb50da4
donnyeonglee <dlehssud@snu.ac.kr>
2025-10-23 13:44:27 +0900

    feat: 경주할 자동차 이름을 입력받는 기능 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,14 @@
 package racingcar;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        // 1. 경주할 자동차 이름을 입력
+        EnterVariable enterVariable = new EnterVariable();
+        List<String> nameList = enterVariable.enterNames();
+        System.out.println("경주할 자동차 이름 List : " + nameList); // 테스트 출력
     }
 }
--- src/main/java/racingcar/EnterVariable.java
+++ src/main/java/racingcar/EnterVariable.java
@@ -1,4 +1,49 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+import java.util.HashSet;
+
 public class EnterVariable {
+
+    public List<String> enterNames() { // 경주할 자동차 이름 입력
+        String inputNames;
+        List<String> nameList = new ArrayList <> ();
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        try {
+            inputNames = Console.readLine();
+            nameList = Arrays.asList(inputNames.split(","));
+
+        } catch (Exception e) {
+            throw new IllegalArgumentException("잘못된 입력입니다.\n에러 메세지 : " + e.getMessage());
+        }
+        for (String name : nameList) {
+            checkBlankName(name);
+            checkNameLength(name);
+        }
+        checkDuplicateName(nameList);
+        return(nameList);
+    }
+
+    private void checkNameLength(String name) { // 이름이 5자 이하인지 확인
+        if (name.length() > 5) {
+            throw new IllegalArgumentException("잘못된 입력입니다.\n에러 메세지 : 이름은 5자 이하만 가능합니다.");
+        }
+    }
+
+    private void checkBlankName(String name) { // 경주할 자동차 이름이 공백, 혹은 빈 이름을 포함하는지 확인
+        if (name.isBlank()) {
+            throw new IllegalArgumentException("잘못된 입력입니다.\n에러 메세지 : 이름은 공백이거나 빈 값일 수 없습니다.");
+        }
+    }
+
+    private void checkDuplicateName(List<String> nameList) { // 경주할 자동차 이름이 중복되는지 확인
+        Set<String> nameSet = new HashSet<>(nameList);
+        if (nameSet.size() != nameList.size()) {
+            throw new IllegalArgumentException("잘못된 입력입니다.\n에러 메세지 : 이름은 중복될 수 없습니다.");
+        }
+    }
 }
