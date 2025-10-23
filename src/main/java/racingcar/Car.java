da76293cca086acd92effbd4cc57237143510594
donnyeonglee <dlehssud@snu.ac.kr>
2025-10-23 15:01:45 +0900

    feat: 자동차의 이름과 위치의 저장 및 출력, 시도에 따른 자동차 움직임 기능 추가

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,4 +1,25 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Car {
+    String name;
+    int position = 0;
+
+    public Car(String name) {
+        this.name = name;
+    }
+
+    public void attempt() {
+        int randomNum = Randoms.pickNumberInRange(0, 9);
+        if (randomNum >= 4) {
+            this.position+=1;
+        }
+        System.out.println("자동차 이름 : ", + name + " 무작위 값 : " + randomNum + " 현재 위치 : " + position); //테스트 출력
+    }
+
+    @Override
+    public String toString() {
+        return name + " : " + "-".repeat(position);
+    }
 }
